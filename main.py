2ff8b3d6f8304467a919b10cd17780b9d8bbf3b7
Brn144 <143962820+Brn144@users.noreply.github.com>
2023-09-09 12:42:10 -0300

    MiniCalculator.py

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-'''
 
-                            Online Python Compiler.
-                Code, Compile, Run and Debug python program online.
-Write your code in this editor and press "Run" button to execute it.
-
-'''
 
 n1 = float(input("Digite o primeiro numero: ")) 
 
@@ -28,16 +22,16 @@ if operacao == "+":
     print("O resultado de {} {} {} = {:.2f}".format(n1, operacao, n2, soma))  
 
 elif operacao == "-":
-    print("O resultado de {} {} {} = {}".format(n1, operacao, n2, subtracao)) 
+    print("O resultado de {} {} {} = {:.2f}".format(n1, operacao, n2, subtracao)) 
     
 elif operacao == "/":
-    print("O resultado de {} {} {} = {}".format(n1, operacao, n2, divisao)) 
+    print("O resultado de {} {} {} = {:.2f}".format(n1, operacao, n2, divisao)) 
     
 elif operacao == "*":
-    print("O resultado de {} {} {} = {}".format(n1, operacao, n2, multiplicacao)) 
+    print("O resultado de {} {} {} = {:.2f}".format(n1, operacao, n2, multiplicacao)) 
 
 elif operacao == "**":
-    print("O resultado de {} {} {} = {}".format(n1, operacao, n2, potencia))
+    print("O resultado de {} {} {} = {:.2f}".format(n1, operacao, n2, potencia))
     
 else:
     print("Error")
